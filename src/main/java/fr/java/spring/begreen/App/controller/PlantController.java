ccaa928b59b90663c91dc90ee98c5c7ef9a41f5e
gitSushi <lossushi37@gmail.com>
2021-08-30 20:12:04 +0000

    getPlants getPlantById working

--- src/main/java/fr/java/spring/begreen/App/controller/PlantController.java
+++ src/main/java/fr/java/spring/begreen/App/controller/PlantController.java
@@ -1,8 +1,13 @@
 package fr.java.spring.begreen.App.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.java.spring.begreen.App.model.Plant;
@@ -15,20 +20,25 @@ public class PlantController {
     @Autowired
     PlantRepository plantRepository;
 
-    @GetMapping(path = "/plants/test")
-    public Plant getNewPlants() {
-        Plant p1 = new Plant();
-        p1.setId(1L);
-        p1.setName("rose");
-        p1.setLatin("rosa");
-        p1.setDescription("Some flower that gets you grls !");
-        this.plantRepository.save(p1);
-        return p1;
+    @PostMapping(path = "/plants/init")
+    public void getInitPlants(@RequestBody Plant[] initPlants) {
+        ArrayList<Plant> plants = new ArrayList<Plant>();
+
+        for (int i = 0; i < initPlants.length; ++i) {
+            plants.add(initPlants[i]);
+        }
+
+        this.plantRepository.saveAll(plants);
     }
 
     @GetMapping(path = "/plants")
     public Iterable<Plant> getPlants() {
         return this.plantRepository.findAll();
+    }
+
+    @GetMapping(path = "/plants/{id}")
+    public Plant getPlantById(@PathVariable(value = "id") Long id) {
+        return this.plantRepository.findById(id).get();
     }
 
 }
--- src/main/java/fr/java/spring/begreen/App/model/Plant.java
+++ src/main/java/fr/java/spring/begreen/App/model/Plant.java
@@ -1,6 +1,8 @@
 package fr.java.spring.begreen.App.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
@@ -10,6 +12,7 @@ import lombok.Data;
 public class Plant {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
