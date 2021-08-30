c8d4af390fd8439514cbde192ff80e000af19050
Alexandre Labsi <alex.lbs2001@gmail.com>
2021-08-30 08:12:58 +0000

    init

---
+++ src/main/java/fr/java/spring/begreen/App/BegreenApplication.java
@@ -0,0 +1,13 @@
+package fr.java.spring.begreen.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BegreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BegreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/java/spring/begreen/begreen/BegreenApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.java.spring.begreen.begreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BegreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
